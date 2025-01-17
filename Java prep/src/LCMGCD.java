a9f914d27634163b8e46b08f7cc2107fd08527a9
Arun R <153985734+itsme-arun@users.noreply.github.com>
2025-01-17 23:57:15 +0530

    Add files via upload

---
+++ Java prep/src/LCMGCD.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LCMGCD {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter First Number:");
+        int num1 = in.nextInt();//user input for first number
+        System.out.print("Enter Second Number:");
+        int num2 = in.nextInt();//user input for second number
+        int gcd = gcd(num1, num2);//calling the gcd function
+        int lcm = (num1 * num2)/gcd;//formula for calculating lcm using gcd
+        System.out.println("GCD:" + gcd);
+        System.out.println("LCM:" + lcm);
+    }
+    // Method to calculate GCD using Euclidean Algorithm
+    public static int gcd(int a, int b){
+        while(b!=0){ //when b becomes zero it will return the value that is stored in 'a' which is the gcd of the given numbers
+            int temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
+}
